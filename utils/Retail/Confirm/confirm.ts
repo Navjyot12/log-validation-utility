3e592e58405f675f92fc53743af64de45e08b724
Navjyot <navjyots.dhaliwal@thewitslab.com>
2025-01-02 18:42:59 +0530

    Payment settlement details provided by the buyer app in /confirm and commented out from on_init

--- utils/Retail/Confirm/confirm.ts
+++ utils/Retail/Confirm/confirm.ts
@@ -318,7 +318,72 @@ export const checkConfirm = (data: any, msgIdSet: any) => {
     } catch (error: any) {
       logger.error(`!!Error while Comparing Quote object for /${constants.ON_SELECT} and /${constants.CONFIRM}`)
     }
-
+    try {
+      logger.info(`checking payment object in /${constants.CONFIRM}`)
+      if (confirm.payment['@ondc/org/settlement_details'][0]['settlement_counterparty'] !== 'buyer-app') {
+        cnfrmObj.sttlmntcntrparty = `settlement_counterparty is expected to be 'buyer-app' in @ondc/org/settlement_details`
+      }
+    
+      logger.info(`checking payment details in /${constants.CONFIRM}`)
+      const data = confirm.payment['@ondc/org/settlement_details'][0]
+      if (
+        data['settlement_type'] !== 'neft' &&
+        data['settlement_type'] !== 'rtgs' &&
+        data['settlement_type'] !== 'upi'
+      ) {
+        logger.error(
+          `settlement_type is expected to be 'neft/rtgs/upi' in @ondc/org/settlement_details in /${constants.CONFIRM}`,
+        )
+        cnfrmObj.sttlmntcntrparty = `settlement_type is expected to be 'neft/rtgs/upi' in @ondc/org/settlement_details`
+      } else if (data['settlement_type'] !== 'upi') {
+        let missingFields = []
+        if (!data.bank_name) {
+          missingFields.push('bank_name')
+        }
+        if (!data.branch_name) {
+          missingFields.push('branch_name')
+        }
+        if (!data.beneficiary_name || data.beneficiary_name.trim() === '') {
+          missingFields.push('beneficiary_name')
+        }
+        if (!data.settlement_phase) {
+          missingFields.push('settlement_phase')
+        }
+        if (!data.settlement_ifsc_code) {
+          missingFields.push('settlement_ifsc_code')
+        }
+        if (!data.settlement_counterparty) {
+          missingFields.push('settlement_counterparty')
+        }
+        if (!data.settlement_bank_account_no || data.settlement_bank_account_no.trim() === '') {
+          missingFields.push('settlement_bank_account_no')
+        }
+    
+        if (missingFields.length > 0) {
+          logger.error(`Payment details are missing: ${missingFields.join(', ')} /${constants.CONFIRM}`)
+          cnfrmObj.paymentDetails = `Payment details are missing: ${missingFields.join(', ')} /${constants.CONFIRM}`
+        }
+      } else {
+        if (!data.upi_address || data.upi_address.trim() === '') {
+          logger.error(`Payment details are missing /${constants.CONFIRM}`)
+          cnfrmObj.paymentDetails = `Payment details are missing /${constants.CONFIRM}`
+        }
+      }
+    } catch (error: any) {
+      logger.error(`!!Error while checking payment object in /${constants.CONFIRM}`)
+    }
+    
+    try {
+      logger.info(`storing payment settlement details in /${constants.CONFIRM}`)
+      if (confirm.payment.hasOwnProperty('@ondc/org/settlement_details'))
+        setValue('sttlmntdtls', confirm.payment['@ondc/org/settlement_details'][0])
+      else {
+        cnfrmObj.pymntSttlmntObj = `payment settlement_details missing in /${constants.CONFIRM}`
+      }
+    } catch (error: any) {
+      logger.error(`!!Error while storing payment settlement details in /${constants.CONFIRM}`)
+    }
+    
     try {
       logger.info(`Checking quote breakup prices for /${constants.CONFIRM}`)
       if (!sumQuoteBreakUp(confirm.quote)) {
